9c18d882e2e36f57c81fad69d285902ebb7e6e2f
Shajan Jacob <shajanjp@gmail.com>
2019-11-04 17:15:57 +0530

    add server controller

---
+++ server.js
@@ -0,0 +1,29 @@
+let express = require('express');
+let bodyParser = require('body-parser');
+let app = express();
+let http = require('http').Server(app);
+let io = require('socket.io')(http);
+let PORT = process.env.PORT || 3000;
+let sessionController = require('./controllers/session-controller.js');
+
+app.set('view engine', 'ejs');
+
+app.get('/view/:sessionId', sessionController.viewHandler);
+
+app.param('sessionId', sessionController.validateSession);
+
+io.on('connection', (client) => {
+  console.log('client connected');
+
+  client.on('PROCESS_OUT', (data) => {
+    io.emit(data.sessionId, data);
+  })
+
+  client.on('disconnect', () => {
+    console.log('disconnected');
+  });
+})
+
+http.listen(PORT, () => {
+  console.log(`Server started at: http://localhost:${PORT}`);
+});
